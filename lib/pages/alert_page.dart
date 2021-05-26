05db69119d86222a1bbb05c8973f51b2ba032cf7
Luis Morelos <lmoreloss@gmail.com>
2021-05-25 20:37:27 -0500

    Añadidos los dialogos

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -8,6 +8,90 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: ListView(
+        children: [
+
+
+          ListTile(
+            title: Text('SimpleDialog'),
+            leading: Icon(Icons.stay_primary_landscape, color: Colors.blue,),
+            trailing: Icon(Icons.chevron_right_sharp),
+            onTap: () {
+               showDialog(context: context, builder: (BuildContext context)=>
+                SimpleDialog(
+                 title: Text('Este es un dialogo simple'),
+                 children: <Widget>[
+                   SimpleDialogOption(
+                      onPressed: (){
+                          Navigator.of(context).pop();
+                        },
+                      child: Text('Cerrar'),
+                    ),
+                  ],
+                )
+               );
+            },
+          ),
+
+
+          Divider(),
+          
+
+          ListTile(
+            title: Text('AlertDialog'),
+            leading: Icon(Icons.battery_alert, color: Colors.blue,),
+            trailing: Icon(Icons.chevron_right_sharp),
+            onTap: () {
+               showDialog(context: context, builder: (BuildContext context)=>
+                AlertDialog(
+                  title: Text('Este es un dialogo de alerta'),  
+                  content: Text('Subtexto'),   
+                   actions: [
+                   FlatButton(
+                     onPressed: () {
+                       Navigator.of(context).pop();
+                     },
+                      child: Text('Aceptar'),
+                    ),
+                 ],
+               ),
+               );
+            },
+          ),
+
+
+          Divider(),
+
+
+          ListTile(
+            title: Text('ShowDialog'),
+            leading: Icon(Icons.slideshow, color: Colors.blue,),
+            trailing: Icon(Icons.chevron_right_sharp),
+            onTap: () {
+               showDialog(
+                  context: context, builder: (BuildContext context) {
+                    return Expanded(
+                      child: AlertDialog(
+                        title: Text('Este es un ShowDialog'),
+                        content: Text('Subtexto'),
+                        actions: [
+                          FlatButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Aceptar'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+            },
+          ),
+
+
+        ],
+      ),
     );
   }
 }
--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -8,6 +8,62 @@ class AvatarPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Avatar Page'),
       ),
+      body: ListView(
+        children: <Widget>[
+
+          GestureDetector(
+            onTap: () {
+              showDialog(context: context, builder: (BuildContext context)=>_buildPopUpDialog(context, NetworkImage('https://cdn.icon-icons.com/icons2/195/PNG/256/YouTube_23392.png')));
+            },
+            child: CircleAvatar(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://cdn.icon-icons.com/icons2/195/PNG/256/YouTube_23392.png'),
+              )
+            )
+          ),
+
+          GestureDetector(
+            onTap: () {
+              showDialog(context: context, builder: (BuildContext context)=>_buildPopUpDialog(context, NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Wikipedia_article_icon_BLACK.svg/1200px-Wikipedia_article_icon_BLACK.svg.png')));
+            },
+            child: CircleAvatar(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Wikipedia_article_icon_BLACK.svg/1200px-Wikipedia_article_icon_BLACK.svg.png'),
+              )
+            )
+          ),
+
+
+
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPopUpDialog(BuildContext context, NetworkImage icono) { //se crea un widget de nombre _buuildPopUpDialog con los parametros BuildContext y String
+    return new AlertDialog( //se regresa un nuevo dialogo de alerta
+      title: const Text('Me diste click'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+              child: CircleAvatar(
+                backgroundImage: icono,
+                radius: 500.0,
+              )
+            )
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton( //se crea un flatbutton
+          onPressed: () { //cuando se presiona
+            Navigator.of(context).pop(); //revienta el mensaje
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Cerrar'),
+        ),
+      ],
     );
   }
 }
